31119cddbe88ccb5c148a9b7db44fd854ca2f240
Jofred Reinosa <reinosaj@gmail.com>
2021-01-28 14:34:55 -0300

    ajustes de estilos

--- src/app/individual-comic/individual-comic.component.ts
+++ src/app/individual-comic/individual-comic.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class IndividualComicComponent implements OnInit {
   @Input() comic;
   thumbnail: string;
+  imageLoader: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
